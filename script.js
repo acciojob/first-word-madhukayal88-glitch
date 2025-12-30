da96318339ee41881a8da94e6e7695e724556708
madhukayal88-glitch <madhukayal88@gmail.com>
2025-12-30 20:09:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-function firstWord(s) {
-  // your code here
+function firstWord(str) {
+  // If the string is empty, return it immediately
+  if (!str) {
+    return "";
+  }
+
+  // 1. Remove leading spaces using trimStart()
+  // 2. split(' ') creates an array of substrings separated by spaces
+  // 3. [0] retrieves the very first element of that array
+  return str.trimStart().split(' ')[0];
 }
 
-// Do not change the code below
-
-const s = prompt("Enter String:");
-alert(firstWord(s));
+// Example usage:
+// console.log(firstWord('see and stop')); // 'see'
+// console.log(firstWord(' Hello World!')); // 'Hello'
